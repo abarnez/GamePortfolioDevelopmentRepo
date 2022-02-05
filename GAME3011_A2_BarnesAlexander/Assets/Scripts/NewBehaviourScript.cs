b61d2f2072c00c3a21a28264d3386a924d1a1b2a
abarnez <alexander_barnes@outlook.com>
2022-02-05 02:02:25 -0500

    key turning mechanic

--- GAME3011_A2_BarnesAlexander/Assets/Scripts/NewBehaviourScript.cs
+++ GAME3011_A2_BarnesAlexander/Assets/Scripts/NewBehaviourScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public GameObject keyHole;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,21 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.rotation.eulerAngles.z <= 90 + 1)
+        {
+            if (transform.rotation.eulerAngles.z >= 90 - 1) {
+                if (Input.GetKey(KeyCode.A))
+                {
+                    keyHole.transform.Rotate(new Vector3(0, 50, 0) * Time.deltaTime);
+                    Debug.Log(transform.rotation.eulerAngles.z);
+                }
+                if (Input.GetKey(KeyCode.D))
+                {
+                    keyHole.transform.Rotate(new Vector3(0, -50, 0) * Time.deltaTime);
+                    Debug.Log(transform.rotation.eulerAngles.z);
+                }
+            }
+        }
         //rotation
         Vector3 mousePos = Input.mousePosition;
         mousePos.z = 5.23f;
