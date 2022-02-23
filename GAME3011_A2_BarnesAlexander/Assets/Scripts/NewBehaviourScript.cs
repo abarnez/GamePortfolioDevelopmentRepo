a928eb8085ee42752c25785cd1ab9a2961bb7919
abarnez <alexander_barnes@outlook.com>
2022-02-22 21:47:08 -0500

    ui stuff

--- GAME3011_A2_BarnesAlexander/Assets/Scripts/NewBehaviourScript.cs
+++ GAME3011_A2_BarnesAlexander/Assets/Scripts/NewBehaviourScript.cs
@@ -1,17 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     public GameObject keyHole;
-    public int rand, allowance, playerSkill;
-    public float keyHealth, Timer;
+    public int rand, playerSkill, difficultyLevel, keys;
+    public float keyHealth, Timer, allowance;
+    public Text timer, keyHP, keysLeft, pSkill, Difficulty;
     // Start is called before the first frame update
     void Start()
     {
+        difficultyLevel = Random.Range(0, 4);
         rand = Random.Range(0, 180);
-        allowance = 2;
+        keys = 3;
         keyHealth = 10;
         Timer = 60.0F;
     }
@@ -19,6 +22,12 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timer.text = "Time Remaining: " + Mathf.Round(Timer);
+        keyHP.text = "Key Health: " + Mathf.Round(keyHealth);
+        keysLeft.text = "Keys Remaining: " + keys;
+        pSkill.text = "Skill Level: " + playerSkill;
+        Difficulty.text = "Lock Difficulty: " + difficultyLevel;
+        allowance = 0.1f + 3 - difficultyLevel;
         Timer -= Time.deltaTime;
             if (transform.rotation.eulerAngles.z <= rand + allowance)
             {
@@ -37,25 +46,35 @@ public class NewBehaviourScript : MonoBehaviour
                     }
                 }
             }
-        if(keyHole.transform.rotation.eulerAngles.x > 177 || keyHole.transform.rotation.eulerAngles.x < 0)
-        {
-            Debug.Log("Open");
-            resetLock();
-        }
+   
 
         else
         {
             if (Input.GetKey(KeyCode.A))
             {
                 keyHealth -= Time.deltaTime;
+                if (keyHealth < 0)
+                {
+                    breakKey();
+                }
 
             }
             if (Input.GetKey(KeyCode.D))
             {
                 keyHealth -= Time.deltaTime;
+                if (keyHealth < 0)
+                {
+                    breakKey();
+                }
             }
         }
-        
+
+        if (keyHole.transform.rotation.eulerAngles.x > 177 || keyHole.transform.rotation.eulerAngles.x < 0)
+        {
+            Debug.Log("Open");
+            resetLock();
+        }
+
         //rotation
         Vector3 mousePos = Input.mousePosition;
         mousePos.z = 5.23f;
@@ -73,9 +92,21 @@ public class NewBehaviourScript : MonoBehaviour
 
     void resetLock()
     {
+        playerSkill += 1;
+        difficultyLevel = Random.Range(0, 4);
         rand = Random.Range(0, 180);
-        keyHealth = 10;
         Timer = 60.0F;
         keyHole.transform.rotation = Quaternion.Euler(new Vector3(90, 0, 0));
+    }
+
+    void gameOver()
+    {
+
+    }
+
+    void breakKey()
+    {
+        keys -= 1;
+        keyHealth = 10;
     }
 }
