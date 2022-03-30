d5d82ccc9a943de4b81a938f608c4d946b316602
abarnez <alexander_barnes@outlook.com>
2022-03-30 16:55:40 -0400

    created project, started on sine waves

---
+++ GAME3011_A4_BarnesIdris/Assets/sinewave.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sinewave : MonoBehaviour
+{
+    public float ampliute = 1;
+    public float frequency = 1;
+    public LineRenderer myLine;
+    public bool pCheck;
+    public int Points;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLine = GetComponent<LineRenderer>();
+        if (!pCheck)
+        {
+            
+            ampliute += Random.Range(-1.0f, 1.0f);
+            frequency += Random.Range(0.0f, 1.0f);
+        }
+    }
+    void Draw()
+    {
+        float xStart = 0;
+        float tau = 2 * Mathf.PI;
+        float xFinish = tau;
+
+        myLine.positionCount = Points;
+        for (int currentPoint = 0; currentPoint < Points; currentPoint++)
+        {
+            float progress = (float)currentPoint / (Points - 1);
+            float x = Mathf.Lerp(xStart, xFinish, progress);
+            float y = ampliute * Mathf.Sin(tau * frequency * x + Time.timeSinceLevelLoad);
+            myLine.SetPosition(currentPoint, new Vector3(x, y, 0));
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Draw();
+
+        if (pCheck)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                frequency -= 0.0001f;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                frequency += 0.0001f;
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                ampliute += 0.001f;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                ampliute -= 0.001f;
+            }
+        }
+    }
+}
