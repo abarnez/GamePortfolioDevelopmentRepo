f6e329e297499d71a7258eb3be566d12490222a0
abdulhakeemidr <56049093+abdulhakeemidr@users.noreply.github.com>
2022-04-03 12:15:17 -0400

    Added Start Game Panel

---
+++ GAME3011_A4_BarnesIdris/Assets/StartGame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour
+{
+    [SerializeField]
+    Button startButton;
+
+    void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    void OnEnable()
+    {
+        Time.timeScale = 0;
+        startButton.onClick.AddListener(PlayGame);
+    }
+
+    void Update()
+    {
+        Time.timeScale = 0;
+    }
+
+    void PlayGame()
+    {
+        Time.timeScale = 1;
+        gameObject.SetActive(false);
+    }
+}
